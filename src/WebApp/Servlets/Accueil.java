24506c1bd6784f8b494989cf67b3881836a7ce5f
bou <bouazza.hicham7@gmail.com>
2018-01-26 02:19:46 +0000

    Update# .gitignore

--- src/WebApp/Servlets/Accueil.java
+++ src/WebApp/Servlets/Accueil.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 //@WebServlet("/Accueil")
 public class Accueil extends HttpServlet
 {
-
+//L'accueil
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
